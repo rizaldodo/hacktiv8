6e52edca211fbfb52214157eb9386c647b90bf71
rizaldodo <rizal.dodo@rocketmail.com>
2019-08-27 20:17:25 +0700

    Add file proxytia.js & switch_case.js

---
+++ phase_0/week_02/proxytia.js
@@ -0,0 +1,22 @@
+var nama = 'sapi';
+var peran = 'penyihir';
+
+if (nama === '' && peran === '') {
+    console.log('Nama dan Peran harus di isi !');
+}else if (nama === '') {
+    console.log('Nama harus di isi!');
+}else if (peran === '') {
+    console.log('Halo bro ' + nama + ', Silahkan pilih peran mu untuk memulai game!' )
+}else if (peran === 'ksatria') {
+    console.log('Selamat datang di dunia Proxytia saudara/i ' + nama);
+    console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu !!!!');
+}else if (peran === 'tabib') {
+    console.log('Selamat datang di dunia Proxytia saudara/i ' + nama);
+    console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka !!!!'); 
+}else if (peran === 'penyihir') {
+    console.log('Selamat datang di dunia Proxytia saudara/i ' + nama);
+    console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenaganmu !!!!');
+}else {
+    console.log('Wahai saudara/i ' + nama + 'yang terhormat')
+    console.log('Peran yang anda pilih tidak ada, silahkan pilih salah satu peran anda sebagai ksatria atau tabib, atau penyihir');
+}
---
+++ phase_0/week_02/switch_case.js
@@ -0,0 +1,44 @@
+var tanggal = '1'; //masukkan tanggal dengan angka antara 1 - 31
+var bulan = '1'; //masukkan bulan antara 1 - 12
+var tahun = '2001'; //masukkan tahhun antara 1900 - 2099
+
+switch (bulan){
+  case '1' :
+    console.log(tanggal + ' Januari ' + tahun);
+    break;
+  case '1' : 
+    console.log(tanggal + ' Februari ' + tahun);
+    break;
+  case '1' : 
+    console.log(tanggal + ' Maret ' + tahun);
+    break;
+  case '4' : 
+    console.log(tangal + ' April ' + tahun);
+    break;
+  case '5' : 
+    console.log(tanggal + ' Mei ' + tahun);
+    break;
+  case '6' : 
+    console.log(tanggal + ' Juni ' + tahun);
+    break;
+  case '7' : 
+    console.log(tanggal + ' Juli ' + tahun);
+    break;
+  case '8' : 
+    console.log(tanggal + ' Agustus ' + tahun);
+    break;
+  case '9' : 
+    console.log(tanggal + ' September ' + tahun);
+    break;
+  case '10' : 
+    console.log(tanggal + ' Oktober ' + tahun);
+    break;
+  case '11' : 
+    console.log(tanggal + ' November ' + tahun);
+    break;
+  case '12' : 
+    console.log(tanggal + ' Desember ' + tahun);
+    break; 
+  default :
+    console.log('Masukkan tanggal bulan tahun, contoh: 1 1 2001');
+}
